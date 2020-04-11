cc80c07d65c57a1da0440f9a80cff7462f333cac
shanglidan <2426549808@qq.com>
2020-04-11 15:09:30 +0800

    Update generate_test_json_side.py

--- tools/data_process/generate_test_json_side.py
+++ tools/data_process/generate_test_json_side.py
@@ -17,7 +17,7 @@ def save(images, annotations):
     for k, v in label_ids.items():
         categories.append({"name": k, "id": v})
     ann['categories'] = categories
-    json.dump(ann, open('data/acoustic/train/side/annotations/testA.json', 'w'))
+    json.dump(ann, open('data/acoustic/train/side/annotations/testB.json', 'w'))
 
 
 def test_dataset(im_dir):
@@ -44,6 +44,6 @@ def test_dataset(im_dir):
 
 
 if __name__ == '__main__':
-    test_dir = 'data/acoustic/a-test-image/image/side/'
+    test_dir = 'data/acoustic/image-B-test/image/side/'
     print("generate test json label file.")
     test_dataset(test_dir)
